af11eb8388799fac74f5f7eb15f7bd08f5ab2515
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-25 06:33:07 +0900

    지도 불러오기 구현, 포인트 to 포인트 구현, Directions 구현중

--- Spring/func1-spring/src/main/java/paastaproj/func1spring/controller/LocationController.java
+++ Spring/func1-spring/src/main/java/paastaproj/func1spring/controller/LocationController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import paastaproj.func1spring.domain.Location;
 import paastaproj.func1spring.service.LocationService;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Controller
 public class LocationController {
     private final LocationService locationService;
+
+
 
     public LocationController(LocationService locationService){
         this.locationService=locationService;
@@ -39,4 +42,32 @@ public class LocationController {
         model.addAttribute("locations",locations);
         return "locations/locationList";
     }
+
+    @GetMapping("/maps")
+    public String map(Model model){
+        List<Location> locations = locationService.showAll();
+
+        String lat_src = locations.get(0).getLatitude();
+        //System.out.println(lat_src);
+        String lng_src = locations.get(0).getLongitude();
+        //System.out.println(lng_src);
+        String lat_dest = locations.get(locations.size()-1).getLatitude();
+        //System.out.println(lat_dest);
+        String lng_dest = locations.get(locations.size()-1).getLongitude();
+        //System.out.println(lng_dest);
+
+        String[] path_arr = null;
+
+        try {
+            path_arr = locationService.sendGet("https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start="
+                    + lng_src + "," + lat_src + "&goal=" + lng_dest + "," + lat_dest + "&option=trafast");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("locations",path_arr);
+
+        return "locations/map";
+    }
+
 }
