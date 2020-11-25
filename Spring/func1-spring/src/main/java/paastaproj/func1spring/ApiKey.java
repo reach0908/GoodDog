03598c379a98c6ea8db9baacbe4b6eabab07db2b
김진호 <67333761+reach0908@users.noreply.github.com>
2020-11-25 18:03:34 +0900

    Delete ApiKey.java

--- Spring/func1-spring/src/main/java/paastaproj/func1spring/ApiKey.java
+++
@@ -1,6 +0,0 @@
-package paastaproj.func1spring;
-
-public class ApiKey {
-    public static String NaverID = "fgocun0nnd";
-    public static String NaverSecret = "LbLMWclRbYsdiUvMc7Mjjp86bbi2DLNz6cZgzNyI";
-}
