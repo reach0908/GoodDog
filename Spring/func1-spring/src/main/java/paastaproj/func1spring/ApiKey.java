798dce7d806056e1d8e9c8ef04403211d381102b
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-25 18:07:38 +0900

    깃이그노어 테스트

---
+++ Spring/func1-spring/src/main/java/paastaproj/func1spring/ApiKey.java
@@ -0,0 +1,8 @@
+package paastaproj.func1spring;
+
+public class ApiKey {
+    public static String NaverID = "fgocun0nnd";
+    public static String NaverSecret = "LbLMWclRbYsdiUvMc7Mjjp86bbi2DLNz6cZgzNyI";
+    public static String MyName = "asdasd";
+}
+
